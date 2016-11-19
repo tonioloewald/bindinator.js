e27a115a4296057492fabfe4028bf4f275126305
Tonio Loewald <tonio@loewald.com>
2016-11-19 11:25:08 -0800

    added some docs to b8r.byPath.js added low-level type-check to object path methods which (oddly) seems to create a small performance improvement as well.

--- source/b8r.byPath.js
+++ source/b8r.byPath.js
@@ -1,4 +1,14 @@
 // bindinator.js Copyright (c) 2016 Tonio Loewald
+/**
+	getByPath(obj, path) -- obtains a value inside an object by a path, e.g.
+	getByPath(obj, "foo.bar") is the equivalent of obj.foo.bar
+	if path is not set or is set to '/' then obj is returned.
+
+	setByPath(obj, path, value) -- sets a value inside an object by a path,
+	e.g. setByPath(obj, "foo.bar", 17) is the equivalent of obj.foo.bar = 17.
+
+	Paths include support for array dereferencing: "foo.bar[2]" will work as expected.
+*/
 (function(module){
 
 function getByPath(obj, path) {
@@ -28,9 +38,22 @@ function setByPath(obj, path, val) {
 		obj = obj[key];
 	}
 	if (path[0].substr(-1) === ']') {
-		obj[parseInt(path[0], 10)] = val;
+		obj[parseInt(path[0], 10)] = matchTypes(val, obj[parseInt(path[0], 10)]);
 	} else {
-		obj[path[0]] = val;
+		obj[path[0]] = matchTypes(val, obj[path[0]]);
+	}
+}
+
+function matchTypes(value, oldValue, info) {
+	if(typeof value === typeof oldValue) {
+		return value;
+	} else if (typeof value === 'string' && typeof oldValue === 'number') {
+		return parseFloat(value);
+	} else if (oldValue !== undefined && oldValue !== null) {
+		console.warn('setByPath found non-matching types');
+		return value;
+	} else {
+		return value;
 	}
 }
 
