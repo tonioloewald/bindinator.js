671732d0c1f57873e3d49c091c6c94d5879209b4
Tonio Loewald <tonio@loewald.com>
2016-12-14 02:03:42 -0800

    fixed bugs in big list

--- source/b8r.byPath.js
+++ source/b8r.byPath.js
@@ -11,42 +11,132 @@
 
 	Paths include support for array dereferencing: "foo.bar[2]" will work as expected.
 */
+/* global module, console */
 (function(module){
 
+'use strict';
+
+function pathParts(path) {
+  if (!path || path === '/') {
+    return [];
+  }
+
+  if (Array.isArray(path)) {
+    return path;
+  } else {
+    const parts = [];
+    while (path.length) {
+      var index = path.search(/\[[^\]]+\]/);
+      if(index === -1) {
+        parts.push(path.split('.'));
+        break;
+      } else {
+        const part = path.substr(0, index);
+        path = path.substr(index);
+        if (part) {
+          parts.push(part.split('.'));
+        }
+        index = path.indexOf(']') + 1;
+        parts.push(path.substr(1, index - 2));
+        // handle paths dereferencing array element like foo[0].id
+        if (path.substr(index, 1) === '.') {
+          index += 1;
+        }
+        path = path.substr(index);
+      }
+    }
+    return parts;
+  }
+}
+
 function getByPath(obj, path) {
-	if(path && path !== '/') {
-		path = path.split(/\.|\[/);
-		while (path.length && obj) {
-			var key = path.shift();
-			if (key.substr(-1) === ']') {
-				key = parseInt(key, 10);
-			}
-			obj = obj[key];
-		} 
-	}
-	return obj;
+  console.log(obj, path);
+  const parts = pathParts(path);
+  var found = obj;
+  var i, max_i, j, max_j;
+  for(i = 0, max_i = parts.length; found && i < max_i; i++) {
+    var part = parts[i];
+    if (Array.isArray(part)) {
+      for(j = 0, max_j = part.length; found && j < max_j; j++) {
+        var key = part[j];
+        found = found[key];
+      }
+    } else {
+      if (!found.length) {
+        found = undefined;
+      } else if (path.indexOf('=') > -1) {
+        const [path, value] = part.split('=');
+        const list = found;
+        found = undefined;
+        for(j = 0, max_j = list.length; j < max_j; j++) {
+          const item = list[j];
+          if (getByPath(item, path) + '' == value) {
+            found = item;
+            break;
+          }
+        }
+      } else {
+        j = parseInt(part, 10);
+        found = found[j];
+      }
+    }
+  }
+  return found === undefined ? null : found;
 }
 
 function setByPath(obj, path, val) {
-	path = path.split(/\.|\[/);
-	while (path.length > 1) {
-		var key = path.shift();
-		if (key.substr(-1) === ']') {
-			key = parseInt(key, 10);
-		}
-		if (!obj[key]) {
-			obj[key] = path[0].substr(-1) === ']' ? [] : {};
-		}
-		obj = obj[key];
-	}
-	if (path[0].substr(-1) === ']') {
-		obj[parseInt(path[0], 10)] = matchTypes(val, obj[parseInt(path[0], 10)]);
-	} else {
-		obj[path[0]] = matchTypes(val, obj[path[0]]);
-	}
+  const parts = pathParts(path);
+
+  while(obj && parts.length) {
+    const part = parts.shift();
+    if (typeof part === 'string') {
+      if (!Array.isArray(obj)) {
+        console.error('setByPath failed: expected array, found', obj);
+        throw 'setByPath failed: expected array';
+      }
+      if (part.indexOf('=') > -1) {
+        const [key_path, key_value] = part.split('=');
+        obj = obj.filter(item => getByPath(item, key_path) + '' == key_value)[0]; // jshint ignore:line
+        if (!parts.length) {
+          if (typeof obj === 'object') {
+            Object.assign(obj, val);
+            return true;
+          } else {
+            console.error('setByPath failed): expected object, found', obj);
+            throw 'setByPath failed: expected object';
+          }
+        }
+      } else {
+        const idx = parseInt(part, 10);
+        if (parts.length) {
+          obj = obj[idx];
+        } else {
+          obj[idx] = matchTypes(val, obj[idx]);
+          return true;
+        }
+      }
+    } else if (Array.isArray(part) && part.length) {
+      while(part.length) {
+        var key = part.shift();
+        if (part.length || parts.length) {
+          if (!obj[key]) {
+            obj[key] = {};
+          }
+          obj = obj[key];
+        } else {
+          obj[key] = matchTypes(val, obj[key]);
+          return true;
+        }
+      }
+    } else {
+      console.error('setByPath failed: bad path', path);
+      throw 'setByPath failed';
+    }
+  }
+  throw 'setByPath failed';
 }
 
-function matchTypes(value, oldValue, info) {
+function matchTypes(value, oldValue) {
 	if(typeof value === typeof oldValue) {
 		return value;
 	} else if (typeof value === 'string' && typeof oldValue === 'number') {
@@ -63,6 +153,6 @@ function matchTypes(value, oldValue, info) {
 	}
 }
 
-module.exports = {getByPath, setByPath};
+module.exports = {getByPath, setByPath, matchTypes, pathParts};
 
 }(module));
