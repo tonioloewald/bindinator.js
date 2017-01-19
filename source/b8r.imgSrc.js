6b39e397d0b3aa23e04366e2632d104de501fbdd
Tonio Loewald <tonio@loewald.com>
2017-01-18 23:08:32 -0800

    imgSrc now supports an opacity parameter

--- source/b8r.imgSrc.js
+++ source/b8r.imgSrc.js
@@ -9,15 +9,13 @@ sets the element to `opacity: 0`, and then fads it in when the image
 is loaded.
 */
 
-module.exports = function imageSrc(img, url){
+module.exports = function imageSrc(img, url, opacity){
   if(!getComputedStyle(img).transition) {
     img.style.transition = '0.25s ease-out';
   }
-  if (url && img.getAttribute('src') !== url) {
-    const image = new Image();
-    img.style.opacity = 0;
-    image.src = url;
-    image.onload = () => img.style.opacity = 1;
-    img.setAttribute('src', url);
-  }
+  const image = new Image();
+  img.style.opacity = 0;
+  image.src = url;
+  image.onload = () => img.style.opacity = opacity || 1;
+  img.setAttribute('src', url);
 }
