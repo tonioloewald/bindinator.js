d0393c4b4f5d10cacaf852fd7f86a424428f1668
Tonio Loewald <tonio@loewald.com>
2017-03-10 22:52:20 -0800

    Added docs to registry

--- source/b8r.registry.js
+++ source/b8r.registry.js
@@ -1,12 +1,27 @@
 /**
 # Object Registry
 
+The object registry is an observable object.
+
+    set('root', object_value); // registers the object with the name
+    set('root.path.to.value', new_value); // sets the value
+
+You can set the registry's properties by path. Root level properties must themselves
+be objects, and are referred to as _registry__.
+
+    get('root.path.to.value'); // gets the value
+
+You can call a property by path.
+
+    call('root.path.to.method', ...args); // returns value as appropriate
+
+You can determine if a root level object has been registered:
 */
 /* global module, require, console */
 'use strict';
 
 const {getByPath, setByPath} = require('./b8r.byPath.js');
-const _models = {};
+const registry = {};
 let listeners = []; // { path_string_or_test, callback }
 
 class Listener {
@@ -25,16 +40,16 @@ class Listener {
   }
 }
 
-const get = path => getByPath(_models, path);
+const get = path => getByPath(registry, path);
 
 const set = (path, value, source_element) => {
   const path_parts = path.split('.');
   const model = path_parts[0];
-  if (path_parts.length > 1 && !_models[model]) {
+  if (path_parts.length > 1 && !registry[model]) {
     console.error(`cannot set ${path} to ${value}, ${model} does not exist`);
   } else if (path_parts.length === 1 && typeof value !== 'object') {
     throw 'cannot set ${path}; you can only register objects at root-level';
-  } else if (setByPath(_models, path, value)) {
+  } else if (setByPath(registry, path, value)) {
     touch(path, source_element);
   }
 };
@@ -48,14 +63,44 @@ const call = (path, ...args) => {
   }
 };
 
+/**
+    touch(path [, source_element]); //
+
+Triggers all observers as though the value at path has changed. Useful
+if you change a property independently of the registry.
+*/
 const touch = (path, source_element) => {
   listeners.
       filter(listener => listener.test(path)).
       forEach(listener => listener.callback(path, source_element));
 };
 
+/**
+    observe('root.path.to', callback); // returns a Listener instance
+
+You can observe a path (string). The callback will fire whenever any path matching or starting
+with the string changes. The callback will be passed the exact path that changed.
+
+    observe(/root.path.[^\.]+.value/, callback);
+
+Instead of a constant path, you can pass a RegExp which will fire the callback when a path
+matching the test changes. The callback will be passed the exact path that changed.
+
+    observe(path => path.split('.').length === 1, callback);
+
+Finally you can observe a path test function.
+
+    unobserve(listener);
+
+You can remove a listener (if you kept the reference handy).
+
+    unobserve(test);
+
+You can remove a listener by test, but it will remove _all_ listeners which use that test.
+*/
+
 const observe = (test, callback) => {
-  new Listener(test, callback);
+  return new Listener(test, callback);
 };
 
 const unobserve = test => {
@@ -79,13 +124,28 @@ const unobserve = test => {
   return found;
 };
 
-const models = () => Object.keys(_models);
+/**
+    registered('root'); // => true | false
 
-const registered = path => !!_models[path.split('.')[0]];
+You can get a list of root level objects:
 
+    models();
+
+You can obtain a value using a path.
+*/
+
+const models = () => Object.keys(registry);
+
+const registered = path => !!registry[path.split('.')[0]];
+
+/**
+    remove('root');
+
+You can remove a root-level object ("model").
+*/
 const remove = name => {
-  if (_models[name]) {
-    delete _models[name];
+  if (registry[name]) {
+    delete registry[name];
   } else {
     console.error(`remove model ${name} failed; does not exist`);
   }
