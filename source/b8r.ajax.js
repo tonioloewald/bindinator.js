e4e90c7e3c1d5c09da8ef4127d7c87175b75557c
Tonio Loewald <tonio@loewald.com>
2017-05-03 21:03:48 -0700

    Revert "removed unnecessary anonymous function wrappers and reduced indentation"
    This reverts commit fbffb577618e4bf60e2fbaa117ddab0b693ef744.

--- source/b8r.ajax.js
+++ source/b8r.ajax.js
@@ -18,69 +18,72 @@ b8r.ajax, etc.
 */
 /* global module, console */
 
-'use strict';
+(function(module) {
+  'use strict';
 
-function ajax(url, method, request_data, config) {
-  return new Promise(function(resolve, reject) {
-    config = config || {};
-    if (!config.headers) {
-      config.headers = [];
-    }
-    var request = new XMLHttpRequest();
-    request.open(method || 'GET', url, true);
-    request.onreadystatechange = () => {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        switch (Math.floor(request.status / 100)) {
-          case 0:
-          case 5:
-          case 4:
-            reject(request);
-            break;
-          case 3:
-            // redirect of some kind
-            break;
-          case 2:
-            resolve(request.responseText);
-            break;
+  function ajax(url, method, request_data, config) {
+    return new Promise(function(resolve, reject) {
+      config = config || {};
+      if (!config.headers) {
+        config.headers = [];
+      }
+      var request = new XMLHttpRequest();
+      request.open(method || 'GET', url, true);
+      request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          switch (Math.floor(request.status / 100)) {
+            case 0:
+            case 5:
+            case 4:
+              reject(request);
+              break;
+            case 3:
+              // redirect of some kind
+              break;
+            case 2:
+              resolve(request.responseText);
+              break;
+          }
         }
+      };
+      if (typeof request_data === 'object') {
+        if (method === 'GET') {
+          throw 'GET requests do not support request body data';
+        }
+        request_data = JSON.stringify(request_data);
+        config.headers.push(
+            {prop: 'Content-Type', value: 'application/json; charset=utf-8'});
       }
-    };
-    if (typeof request_data === 'object') {
-      if (method === 'GET') {
-        throw 'GET requests do not support request body data';
-      }
-      request_data = JSON.stringify(request_data);
-      config.headers.push(
-          {prop: 'Content-Type', value: 'application/json; charset=utf-8'});
-    }
-    config.headers.forEach(
-        header => request.setRequestHeader(header.prop, header.value));
-    request.send(request_data);
-  });
-}
+      config.headers.forEach(
+          header => request.setRequestHeader(header.prop, header.value));
+      request.send(request_data);
+    });
+  }
 
-function json(url, method, request_data, config) {
-  return new Promise(function(resolve, reject) {
-    ajax(url, method, request_data, config).then(data => {
-      try {
-        resolve(JSON.parse(data || 'null'));
-      } catch (e) {
-        console.error('Failed to parse data', data, e);
-      }
-    }, reject);
-  });
-}
+  function json(url, method, request_data, config) {
+    return new Promise(function(resolve, reject) {
+      ajax(url, method, request_data, config).then(data => {
+        try {
+          resolve(JSON.parse(data || 'null'));
+        } catch (e) {
+          console.error('Failed to parse data', data, e);
+        }
+      }, reject);
+    });
+  }
 
-function jsonp(url, method, request_data, config) {
-  return new Promise(function(resolve, reject) {
-    ajax(url, method, request_data, config).then(data => {
-      try {
-        resolve(JSON.parse(data || 'null'));
-      } catch (e) {
-        console.error('Failed to parse data', data, e);
-      }
-    }, reject);
-  });
-}
+  function jsonp(url, method, request_data, config) {
+    return new Promise(function(resolve, reject) {
+      ajax(url, method, request_data, config).then(data => {
+        try {
+          resolve(JSON.parse(data || 'null'));
+        } catch (e) {
+          console.error('Failed to parse data', data, e);
+        }
+      }, reject);
+    });
+  }
 
-module.exports = {ajax, json, jsonp};
+  module.exports = {ajax, json, jsonp};
+
+}(module));
