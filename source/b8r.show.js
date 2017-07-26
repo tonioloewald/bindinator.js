e9e79f92a5057100399075a064d8be33e7a4e1a0
Tonio Loewald <tonio@loewald.com>
2017-07-26 15:36:14 -0700

    fix — show needs to send event to the original element (include_self) fix — show needs to call trigger, not dispatch, since show/hide are non-standard events

--- source/b8r.show.js
+++ source/b8r.show.js
@@ -1,26 +1,26 @@
 /**
 # Show and Hide
 */
-/* global module */
+/* global module, require */
 'use strict';
 
 const {findWithin} = require('./b8r.dom.js');
-const {dispatch} = require('./b8r.events.js');
+const {trigger} = require('./b8r.events.js');
 
-const show = element => {
+const show = (element, ...args) => {
   if (element.style.display === 'none') {
     element.style.display = element.getAttribute('data-orig-display') || '';
-    findWithin(element, '[data-event*="show"]')
-        .forEach(elt => dispatch('show', elt));
+    findWithin(element, '[data-event*="show"]', true)
+        .forEach(elt => trigger('show', elt, ...args));
   }
 };
 
-const hide = element => {
+const hide = (element, ...args) => {
   if (element.getAttribute('data-orig-display') === null &&
       (element.style.display && element.style.display !== 'none')) {
     element.setAttribute('data-orig-display', element.style.display);
-    findWithin(element, '[data-event*="hide"]')
-        .forEach(elt => dispatch('hide', elt));
+    findWithin(element, '[data-event*="hide"]', true)
+        .forEach(elt => trigger('hide', elt, ...args));
   }
   element.style.display = 'none';
 };
