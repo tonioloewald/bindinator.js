42c54bec2976102b61315ebbc6e16719cfb07446
Tonio Loewald <tonio@loewald.com>
2017-10-01 18:22:21 -0700

    tweaked documentation

--- source/b8r.makeStylesheet.js
+++ source/b8r.makeStylesheet.js
@@ -3,9 +3,10 @@
 
 Usage:
 
-    makeStylesheet('... css source ...');
+    makeStylesheet('h1 { font-size: 100px; }', 'my style sheet');
 
-Inserts the source in a `<style>` tag and sticks in in the document head.
+Inserts the source in a `<style>` tag and sticks in in the document head. It will have the
+supplied title as its `data-title` attribute;
 */
 /* global module, require */
 'use strict';
@@ -16,7 +17,7 @@ module.exports = (source, title) => {
   const style = source ? create('style') : false;
   if (style) {
     style.type = 'text/css';
-    style.setAttribute('data-title', title);
+    style.dataset.title = title;
     style.appendChild(text(source));
     document.head.appendChild(style);
   }
