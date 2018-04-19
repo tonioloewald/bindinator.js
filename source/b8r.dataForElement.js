dfb3ff694b90fb161d95a2a719ed38f2f71016cb
Tonio Loewald <tonio@loewald.com>
2018-04-18 19:31:52 -0700

    fix — these changes (which will probably cause crashes) have been removed since the filterObject change renders them unnecessary.

--- source/b8r.dataForElement.js
+++ source/b8r.dataForElement.js
@@ -10,28 +10,34 @@ const data_waiting_for_components = [];  // { target_element, data }
 const saveDataForElement = (target_element, data) => {
   if (data) {
     removeDataForElement(target_element);
-    const json = JSON.stringify(data);
-    data_waiting_for_components.push({target_element, data: json});
+    data_waiting_for_components.push({target_element, data});
   }
 };
 
 const removeDataForElement = (target_element) => {
-  const idx = data_waiting_for_components.findIndex(item => item.target_element === target_element);
-  if (idx > -1) {
-    delete data_waiting_for_components[idx];
+  for (var i = 0; i < data_waiting_for_components.length; i++) {
+    if (data_waiting_for_components[i].target_element === target_element) {
+      delete data_waiting_for_components[i].data;
+    }
   }
 };
 
 const dataForElement = (target_element, _default) => {
-  const idx = data_waiting_for_components.findIndex(item => item.target_element === target_element);
-  if (idx === -1) {
-    const json = target_element.dataset.json;
-    return json ? JSON.parse(json) : _default;
-  } else {
-    const {data} = JSON.parse(data_waiting_for_components[idx]);
-    delete data_waiting_for_components[idx];
-    return data;
+  var data;
+  for (var i = 0; i < data_waiting_for_components.length; i++) {
+    if (data_waiting_for_components[i].target_element === target_element) {
+      data = data_waiting_for_components[i].data;
+      removeDataForElement(target_element);
+      return data;
+    }
   }
+
+  const json = target_element.dataset.json;
+  if (json) {
+    return JSON.parse(json);
+  }
+
+  return _default;
 };
 
 module.exports = {
