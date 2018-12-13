b86bfcf5e3e64285316e5796821e0b090890a96a
Tonio Loewald <tonio@loewald.com>
2018-12-13 07:38:24 -0800

    improved web components support
    added web-components.js helper library
    added select.js
    reimplemented switch.js using new library

---
+++ web-components/select.js
@@ -0,0 +1,149 @@
+const {
+  fragment,
+  makeElement,
+  makeWebComponent,
+} = require('../lib/web-components.js');
+
+makeWebComponent('select-option', {
+  value: {writeable: false},
+  style: {
+    ':host': {
+      display: 'inline-block',
+      padding: '3px 5px',
+      height: '100%',
+      margin: 0,
+      borderRadius: '2px',
+    },
+  },
+  eventHandlers: {
+    mouseup(evt) {
+      this.parentElement.value = this.value;
+    },
+  },
+  ariaRole: 'select',
+});
+
+makeWebComponent('select-bar', {
+  value: {
+    writeable: true
+  },
+  attributes: {
+    background: '#ddd',
+    color: 'black',
+    selectedBackground: 'white',
+    selectedColor: 'blue',
+    transition: '0.125s ease-out',
+  },
+  style: {
+    ':host': {
+      display: 'inline-flex',
+      font: '14px Helvetica, Sans-serif',
+      alignItems: 'stretch',
+      borderRadius: '99px',
+      overflow: 'hidden',
+      cursor: 'default',
+      userSelect: 'none',
+      position: 'relative',
+      border: '1px solid transparent',
+    }
+  },
+  methods: {
+    render(){
+      thistabIndex = 0;
+      this.style.background = this.background;
+      this.style.borderColor = this.background;
+      const options = [...this.children].filter(x => x.tagName === 'SELECT-OPTION');
+      options.forEach((option, idx) => {
+        option.style.transition = this.transition;
+        option.style.color = option.value == this.value ? this.selectedColor : this.color;
+        option.style.background = option.value == this.value ? this.selectedBackground : '';
+        option.style.paddingLeft = idx === 0 ? '8px' : '';
+        option.style.paddingRight = idx === options.length - 1 ? '8px' : '';
+      });
+    },
+  },
+  ariaRole: 'select',
+});
+
+makeWebComponent('select-pop', {
+  value: {
+    writeable: true
+  },
+  attributes: {
+    background: '#ddd',
+    color: 'black',
+    selectedBackground: 'white',
+    selectedColor: 'blue',
+    transition: '0.125s ease-out',
+    open: false,
+    width: '100px',
+  },
+  style: {
+    ':host': {
+      display: 'inline-block',
+      position: 'relative',
+      cursor: 'default',
+      borderRadius: '3px',
+      border: '1px solid transparent',
+    },
+    '.selection': {
+      display: 'inline-flex',
+    },
+    '.selection > *': {
+      flexGrow: '1',
+    },
+    '.menu': {
+      position: 'absolute',
+      top: '22px',
+      left: '-1px',
+      zIndex: 100,
+      display: 'flex',
+      flexDirection: 'column',
+      font: '14px Helvetica, Sans-serif',
+      alignItems: 'stretch',
+      borderRadius: '2px',
+      overflow: 'hidden',
+      userSelect: 'none',
+      border: '1px solid transparent',
+    }
+  },
+  eventHandlers: {
+    mouseenter(evt) {
+      if (evt.target === this) this.open = true;
+    },
+    mouseleave(evt) {
+      if (evt.target === this) this.open = false;
+    },
+    mouseup(evt) {
+      requestAnimationFrame(() => this.open = !this.open);
+    }
+  },
+  methods: {
+    render(){
+      thistabIndex = 0;
+      const selection = this.shadowRoot.querySelector('.selection');
+      selection.innerHTML = [];
+      selection.style.width = this.width;
+      this.style.background = this.background;
+      this.style.borderColor = this.background;
+      const options = [...this.children].filter(x => x.tagName === 'SELECT-OPTION');
+      const menu = this.shadowRoot.querySelector('.menu');
+      menu.style.display = this.open ? '' : 'none';
+      menu.style.background = this.background;
+      menu.style.width = this.width;
+      console.log(this.open, menu);
+      options.forEach((option, idx) => {
+        const selected = option.value == this.value;
+        option.style.transition = this.transition;
+        option.style.color = selected ? this.selectedColor : this.color;
+        option.style.background = selected ? this.selectedBackground : '';
+        if (selected) selection.appendChild(option.cloneNode(true));
+      });
+    },
+  },
+  content: fragment(
+    makeElement('div', {classes: ['selection']}), 
+    makeElement('div', {classes: ['menu'], content: makeElement('slot', {})}),
+  ),
+  ariaRole: 'select',
+});
--- web-components/switch.js
+++ web-components/switch.js
@@ -1,122 +1,65 @@
-const style = document.createElement('style');
-style.textContent = `
-  :host {
-    display: inline-block;
-    width: 28px;
-    height: 14px;
-    margin: 2px 0 0;
-    border-radius: 99px;
-    box-shadow: inset 0 1px 2px rgba(0,0,0,0.25);
-    cursor: default;
-  }
+const {
+  makeElement,
+  makeWebComponent,
+} = require('../lib/web-components.js');
 
-  .thumb {
-    content: ' ';
-    display: inline-block;
-    width: 14px;
-    height: 14px;
-    margin: -1px;
-    border: 1px solid #ddd;
-    background: #eee;
-    border-radius: 99px;
-    box-shadow:
-      0 1px 2px rgba(0,0,0,0.5),
-      inset 0 -1px 1px rgba(0,0,0,0.125);
-  }
-
-  .thumb.on {
-    margin-left: 12px;
-  }
-`;
-
-const div = document.createElement('div');
-div.innerHTML = `<span class="thumb"></span>`;
-
-const dom = div.children[0];
-
-class SlideToggle extends HTMLElement {
-  constructor () {
-    super();
-
-    const shadow = this.attachShadow({ mode: 'open' });
-
-    shadow.appendChild(style.cloneNode(true));
-    this.thumb = dom.cloneNode(true);
-    shadow.appendChild(this.thumb);
-    this.addEventListener('mouseup', this.mouseup.bind(this));
-    this.addEventListener('keyup', this.keyup.bind(this));
-
-    this.tabIndex = 0;
-    this.setAttribute('aria-role', 'switch');
-    this.render();
-  }
-
-  // add animated transitions only after user starts monkeying with control
-  // so it doesn't animate into initial state
-  addTransition () {
-    this.style.transition = '0.25s ease-out';
-    this.thumb.style.transition = '0.25s ease-out';
-  }
-
-  mouseup () {
-    if (! this.disabled) {
-      this.addTransition();
-      this.value = ! this.value;
+makeWebComponent('checkbox-switch', {
+  value: {
+    writeable: true
+  },
+  attributes: {
+    color: '#ccc',
+    onColor: '#0f0',
+    transition: '0.125s ease-out',
+    thumbColor: '#eee',
+    width: '36px',
+    height: '16px',
+    thumbSize: '24px',
+  },
+  style: {
+    ':host': {
+      position: 'relative',
+      display: 'inline-block',
+      borderRadius: '99px',
+      cursor: 'default',
+    },
+    '.thumb': {
+      position: 'absolute',
+      display: 'block',
+      borderRadius: '99px',
+    },
+  },
+  content: makeElement('span', {classes: ['thumb']}),
+  eventHandlers: {
+    mouseup(evt) {
+      this.value = !this.value;
+    },
+    keydown(evt) {
+      evt.preventDefault();
+    },
+    keyup(evt) {
+      if (evt.code === 'Space') this.value = !this.value;
+      evt.preventDefault();
     }
-  }
-
-  keyup (evt) {
-    if (! this.disabled && evt.code === 'Space') {
-      this.addTransition();
-      this.value = ! this.value;
-    }
-  }
-
-  get onColor () {
-    return this.getAttribute('onColor') || '#0f0';
-  }
-
-  set onColor (c) {
-    this.setAttribute('onColor', c);
-  }
-
-  get offColor () {
-    return this.getAttribute('offColor') || '#ccc';
-  }
-
-  set offColor (c) {
-    this.setAttribute('offColor', c);
-  }
-
-  get disabled () {
-    return this.hasAttribute('disabled');
-  }
-
-  set disabled (x) {
-    if (x) {
-      this.setAttribute('disabled', '');
-    } else {
-      this.removeAttribute('disabled');
-    }
-  }
-
-  get value () {
-    return this.hasAttribute('value') && this.getAttribute('value') !== 'false';
-  }
-
-  set value (x) {
-    x = x && x !== 'false';
-    if (this.value !== x) {
-      this.setAttribute('value', x);
-      this.render();
-      this.dispatchEvent(new Event('change'));
-    }
-  }
-
-  render () {
-    this.setAttribute('aria-checked', this.value ? 'on' : 'off');
-    this.thumb.classList.toggle('on', this.value);
-    this.style.backgroundColor = this.value ? this.onColor : this.offColor;
-  }
-}
-window.customElements.define('b8r-switch', SlideToggle);
+  },
+  methods: {
+    render() {
+      this.tabIndex = 0;
+      const thumb = this.shadowRoot.querySelector('.thumb');
+      thumb.style.transition = this.transition;
+      thumb.style.background = this.thumbColor;
+      thumb.style.width = this.thumbSize;
+      thumb.style.height = this.thumbSize;
+      const height = parseFloat(this.height);
+      const thumbSize = parseFloat(this.thumbSize);
+      const inset = (height - thumbSize) * 0.5;
+      thumb.style.top = `${inset}px`;
+      thumb.style.left = this.value ? `${this.offsetWidth - inset - thumbSize}px` : `${inset}px`;
+      this.style.margin = `${-inset}px`;
+      this.style.background = this.value ? this.onColor : this.color,
+      this.style.width = this.width;
+      this.style.height = this.height;
+      this.style.transition = this.transition;
+    },
+  },
+});
