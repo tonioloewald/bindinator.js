2945759ff04b8ebc684f29786c9610d1b8f073d2
Tonio Loewald <tonio@loewald.com>
2019-01-04 22:28:57 -0800

    dialog and bindery components

---
+++ web-components/bindery.js
@@ -0,0 +1,107 @@
+/**
+# bindery
+
+This is a simple experimental component for binding a model to the DOM. 
+You assign an object to the `<bindery-model>`'s `value` and then bind to its 
+(top-level) properties by name.
+
+For example:
+
+```
+<bindery-model events="mouseup">
+  <h4>Simple Binding</h4>
+  <button data-on="mouseup=click" data-to="textContent=caption"></button>
+  <h4>One-Way Binding</h4>
+  <label>
+    to-binding
+    <input data-to="value=caption"><button data-on="mouseup=save">Save</button>
+  </label>
+  <h4>Two-Way Binding</h4>
+  <label>
+    from-binding
+    <input data-from="value=caption">
+  </label>
+</bindery-model>
+<script>
+  require('web-components/bindery.js');
+  document.querySelector('bindery-model').value = {
+    caption: 'an example',
+    click () {
+      alert(`you clicked "${this.value.caption}"`)
+    },
+    save (evt) {
+      const caption = evt.target.previousElementSibling.value;
+      this.value = Object.assign(this.value, {caption});
+    },
+  };
+</script>
+```
+
+Bindery uses `data-on`, `data-to`, and `data-from` attributes to drive its bindings.
+
+- `data-on` binds events to methods by name
+- `data-to` sends data to the DOM by name (one-way binding)
+- `data-from` syncs data to the DOM by name (two-way binding)
+
+As of now, this is really just a toy. It only allows for one event and one data
+binding per element, and it doesn't use b8r's `byPath` library or its `toTargets`
+and `fromTargets`. But it's fairly easy to see that if this functionality were
+integrated, then a `<bindery-model>` element would function as a standalone 
+`b8r`-like context.
+
+Similarly, it would be easy enough to make the `data-from` bindings inactive by
+default (essentially giving you a redux-like data-flow) or simply not support them
+at all.
+*/
+
+const {
+  makeWebComponent,
+} = require('../lib/web-components.js');
+
+const BinderyModel = makeWebComponent('bindery-model', {
+  value: true,
+  attributes: {
+    events: '',
+  },
+  methods: {
+    get(path) {
+      return this.value[path];
+    },
+    handleEvent(evt) {
+      const bindery = evt.target.closest('bindery-model');
+      const model = bindery.value;
+      const {type, target} = evt;
+      const eventTarget = target.closest(`[data-on*="${type}"]`);
+      if (eventTarget) {
+        const handler = eventTarget.dataset.on.split('=').pop();
+        model[handler].call(bindery, evt, eventTarget, model);
+      }
+    },
+    handleChange(evt) {
+      const bindery = evt.target.closest('bindery-model');
+      const {type, target} = evt;
+      const changeTarget = target.closest(`[data-from]`);
+      if (changeTarget) {
+        const [target, path] = changeTarget.dataset.from.split('=');
+        bindery.value = Object.assign(bindery.value, {[path]: changeTarget[target]})
+      }
+    },
+    render() {
+      const slot = this.shadowRoot.querySelector('slot');
+      this.events.split(',').forEach(type => slot.addEventListener(type, this.handleEvent, {capture:true}));
+      ['change', 'input'].forEach(type => slot.addEventListener(type, this.handleChange, {capture:true}));
+      const model = this.value;
+      if (model) {
+        const subscribers = this.querySelectorAll('[data-to],[data-from]');
+        subscribers.forEach(elt => {
+          const [prop, path] = (elt.dataset.to || elt.dataset.from).split('=');
+          elt[prop] = model[path];
+        });
+      }
+    }
+  },
+});
+
+module.exports = {
+  BinderyModel,
+}
---
+++ web-components/dialog.js
@@ -0,0 +1,261 @@
+/**
+# Dialog
+
+`<dialog-modal>` simply places its content in a centered box with a translucent dark
+overlay covering the rest of the window.
+
+By default, the dialog will initially be hidden. To show it, set its `active` attribute
+to `true` or call its `show()` method.
+
+The element has several methods:
+
+- `addCallback` adds a listener that will be passed the value of the dialog when it's closed.
+- `close(value)` closes (hides) the dialog and sets its value
+- `show` and `hide` do the obvious.
+
+Note that the contents of the dialog are simply whatever you put in them. There's no magic.
+
+```
+    <style>
+      .dialog-modal-frame {
+        display: flex;
+        flex-direction: column;
+      }
+
+      .dialog-modal-title {
+        padding: 5px 10px;
+        font-weight: bold;
+        background: #eee;
+      }
+
+      .dialog-modal-input {
+        padding: 2px 4px;
+        margin: 5px 10px;
+      }
+
+      .dialog-modal-message {
+        margin: 5px 10px;
+      }
+
+      .dialog-modal-button-set {
+        text-align: right;
+        margin: 0 10px 10px;
+      }
+
+      .dialog-modal-button + .dialog-modal-button {
+        margin-left: 5px;
+      }
+    </style>
+    <dialog-modal>
+      <div class="dialog-modal-frame">
+        <div class="dialog-modal-title">Modal</div>
+        <div class="dialog-modal-message">Here is some text</div>
+        <label class="dialog-modal-message">
+          Text field
+          <input placeholder="type something" data-event="
+            keydown(Escape):dialog-demo.cancel;
+            keydown(Enter):dialog-demo.ok;
+          ">
+        </label>
+        <div class="dialog-modal-button-set">
+          <button data-event="click:dialog-demo.cancel">Cancel</button>
+          <button data-event="click:dialog-demo.ok">OK</button>
+        </div>
+      </div>
+    </dialog-modal>
+    <p data-bind="json=dialog-demo.result"></p>
+    <button data-event="click:dialog-demo.showDialog">Show Dialog</button>
+    <button data-event="click:dialog-demo.showAlert">dialogAlert</button>
+    <button data-event="click:dialog-demo.showConfirm">dialogConfirm</button>
+    <button data-event="click:dialog-demo.showPrompt">dialogPrompt</button>
+    <script>
+      const {
+        dialogAlert,
+        dialogConfirm,
+        dialogPrompt,
+      } = require('web-components/dialog.js');
+      const dialog = findOne('dialog-modal');
+      const input = dialog.querySelector('input');
+      dialog.addCallback(value => b8r.set('dialog-demo.result', value));
+      b8r.register('dialog-demo', {
+        result: {},
+        showDialog(){
+          dialog.show();
+        },
+        async showAlert(){
+          b8r.set('dialog-demo.result', null);
+          const buttonClicked = await dialogAlert('This is a test');
+          b8r.set('dialog-demo.result', {buttonClicked});
+        },
+        async showConfirm(){
+          b8r.set('dialog-demo.result', null);
+          const buttonClicked = await dialogConfirm('This is a test');
+          b8r.set('dialog-demo.result', {buttonClicked});
+        },
+        async showPrompt(){
+          b8r.set('dialog-demo.result', null);
+          const text = await dialogPrompt('Edit this', 'some text');
+          b8r.set('dialog-demo.result', {text});
+        },
+        cancel(){
+          dialog.close({text: null, buttonClicked: 'cancel'});
+        },
+        ok(){
+          dialog.close({text: input.value, buttonClicked: 'ok'});
+        },
+      });
+    </script>
+```
+*/
+
+const {
+  makeWebComponent,
+  makeElement,
+  fragment,
+  div,
+  input,
+  button,
+} = require('../lib/web-components.js');
+
+const DialogModal = makeWebComponent('dialog-modal', {
+  value: true,
+  attributes: {
+    active: false,
+  },
+  props: {
+    callbacks: [],
+  },
+  methods: {
+    addCallback(fn) {
+      this.callbacks.push(fn);
+    },
+    close(value) {
+      if (value !== undefined) this.value = value; 
+      this.callbacks.forEach(callback => callback(this.value));
+      this.active = false;
+    },
+    show() {
+      this.active = true;
+    },
+    hide() {
+      this.active = false;
+    },
+    handleShortcut(evt) {
+      const {key} = evt;
+      const button = this.querySelector(`.dialog-modal-button[data-shortcut="${key}"]`);
+      if (button) {
+        button.click();
+        evt.stopPropagation();
+        evt.preventDefault();
+      }
+    },
+    render() {
+      let display = 'none';
+      if (!this.hasAttribute('tabindex')) {
+        this.setAttribute('tabindex', 0);
+        this.addEventListener('keydown', this.handleShortcut.bind(this)); 
+      }
+      if (this.active) {
+        display = '';
+        requestAnimationFrame(() => {
+          const elt = this.querySelector('input,textarea,button,select,[tabindex]');
+          if (elt) elt.focus();
+        });
+      }
+      this.shadowRoot.querySelector('slot').style.display = display;
+      this.style.display = display;
+    },
+  },
+  style: {
+    ':host': {
+      display: 'block',
+      position: 'fixed',
+      background: 'rgba(0,0,0,0.25)',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      zIndex: 100,
+    },
+    'slot': {
+      display: 'block',
+      position: 'fixed',
+      background: 'white',
+      left: '50%',
+      top: '50%',
+      transform: 'translateX(-50%) translateY(-50%)',
+      borderRadius: '4px',
+      boxShadow: '0 10px 20px 0 rgba(0,0,0,0.5)',
+    },
+  },
+});
+
+const _ok_button = {name: 'OK', keystroke: 'Enter'};
+
+const _confirm_buttons = [
+  _ok_button,
+  {name: 'Cancel', keystroke: 'Escape'},
+];
+
+const _makeButtons = (buttons) => 
+  buttons.map(({name, keystroke}) => button({
+    content: name,
+    classes: ['dialog-modal-button'],
+    attributes: {
+      title: `[${keystroke}] ${name}`,
+      'data-shortcut': keystroke,
+    }
+  }));
+
+const dialogAlert = (message, title=window.location.host) => dialogConfirm(message, title, [_ok_button]);
+
+const dialogConfirm = (message, title=window.location.host, buttons=_confirm_buttons) => 
+  new Promise((resolve, reject) => {
+    const dialog = new DialogModal();
+    const _title = div({content: title, classes: ['dialog-modal-title']});
+    const _message = div({content: message, classes: ['dialog-modal-message']});
+    const _buttonSet = div({content: _makeButtons(buttons), classes: ['dialog-modal-button-set']});
+    const _frame = div({
+      content: [_title, _message, _buttonSet],
+      classes: ['dialog-modal-frame'],
+    });
+    const _action = (evt) => {
+      dialog.remove();
+      resolve(evt.target.textContent);
+    };
+    dialog.appendChild(_frame);
+    [...dialog.querySelectorAll('.dialog-modal-button')].forEach(_button => _button.addEventListener('click', _action));
+    document.body.appendChild(dialog);
+    dialog.show();
+  });
+
+const dialogPrompt = (message='Enter some text', text='', title=window.location.host) => 
+  new Promise((resolve, reject) => {
+    const dialog = new DialogModal();
+    const _title = div({content: title, classes: ['dialog-modal-title']});
+    const _message = div({content: message, classes: ['dialog-modal-message']});
+    const _input = input({classes: ['dialog-modal-input']});
+    _input.value = text;
+    const _buttonSet = div({content: _makeButtons(_confirm_buttons), classes: ['dialog-modal-button-set']});
+    const _frame = div({
+      content: [_title, _message, _input, _buttonSet],
+      classes: ['dialog-modal-frame'],
+    });
+    dialog.appendChild(_frame);
+    const _action = (evt) => {
+      dialog.remove();
+      resolve(evt.target.textContent === 'OK' ? dialog.querySelector('input').value : null);
+    };
+    [...dialog.querySelectorAll('button')].forEach(_button => _button.addEventListener('click', _action));
+    document.body.appendChild(dialog);
+    dialog.show();
+    dialog.querySelector('input').setSelectionRange(0, 32000);
+  });
+
+module.exports = {
+  DialogModal,
+  dialog,
+  dialogAlert,
+  dialogConfirm,
+  dialogPrompt,
+}
