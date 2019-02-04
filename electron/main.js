27acb02e7f319dc33b42eecd2961f1d0d80a0f8d
Tonio Loewald <tonio@loewald.com>
2019-02-03 20:12:14 -0800

    standard-ized server.js

--- electron/main.js
+++ electron/main.js
@@ -1,9 +1,9 @@
 /* global require, __dirname */
-'use strict';
+'use strict'
 
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
 
 function createWindow () {
   // Create the browser window.
@@ -12,14 +12,14 @@ function createWindow () {
     minHeight: 360,
     width: 800,
     height: 600
-  });
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, '../index.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
--- server.js
+++ server.js
@@ -1,109 +1,109 @@
 /* global require, __dirname */
-'use strict';
+'use strict'
 
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const url = require('url');
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+const url = require('url')
 
 const settings = {
   socket: 8017,
   web_root: __dirname,
   cert_path: 'localhost-ssl/public.pem',
-  key_path: 'localhost-ssl/private.pem',
-};
+  key_path: 'localhost-ssl/private.pem'
+}
 
 process.argv.slice(2).forEach(arg => {
-  const parts = arg.split(':').map(s => s.trim());
+  const parts = arg.split(':').map(s => s.trim())
   if (parts[0]) {
-    settings[parts[0]] = parts.length === 2 ? parts[1] : true;
+    settings[parts[0]] = parts.length === 2 ? parts[1] : true
   }
-});
+})
 
-console.log(settings);
+console.log(settings)
 
-const options = settings.https ? 
-  {
+const options = settings.https
+  ? {
     key: fs.readFileSync(settings.key_path),
-    cert: fs.readFileSync(settings.cert_path),
-  } : 
-  {};
+    cert: fs.readFileSync(settings.cert_path)
+  }
+  : {}
 
-const handler_map = []; // { handler },
-  // handler is a function;
-  // handler.test is an endpoint test,
-  // handler.methods is array of methods
+const handlerMap = [] // { handler },
+// handler is a function;
+// handler.test is an endpoint test,
+// handler.methods is array of methods
 
 const on = (methods, endpoint, handler) => {
-  handler.methods = Array.isArray(methods) ? methods : [methods];
+  handler.methods = Array.isArray(methods) ? methods : [methods]
 
   if (typeof endpoint === 'function') {
-    handler.test = endpoint;
+    handler.test = endpoint
   } else if (endpoint instanceof RegExp) {
-    handler.test = path => endpoint.test(path);
+    handler.test = path => endpoint.test(path)
   } else if (typeof endpoint === 'string') {
-    handler.test = path => path === endpoint;
+    handler.test = path => path === endpoint
   } else {
-    throw 'expect endpoint to be a string, RegExp, or test function';
+    throw new Error('expect endpoint to be a string, RegExp, or test function')
   }
 
-  handler_map.push(handler);
-};
+  handlerMap.push(handler)
+}
 
-const mime_types = {
+const mimeTypes = {
   svg: 'image/svg+xml',
   css: 'text/css',
   jpg: 'image/jpeg',
   png: 'image/png',
   json: 'application/json',
-  js: 'text/javascript',
-};
+  js: 'text/javascript'
+}
 
-const handle_static_request = (req, res) => {
-  const url_obj = url.parse(req.url);
+const handleStaticRequest = (req, res) => {
+  const urlObj = url.parse(req.url)
   if (req.headers.origin) {
-    res.setHeader('Access-Control-Allow-Headers', req.headers.origin); 
+    res.setHeader('Access-Control-Allow-Headers', req.headers.origin)
   }
-  fs.readFile(settings.web_root + url_obj.pathname, (err, data) => {
+  fs.readFile(settings.web_root + urlObj.pathname, (err, data) => {
     if (err) {
-      res.writeHead(404);
-      res.end('not found');
+      res.writeHead(404)
+      res.end('not found')
     } else {
-      const file_extension = url_obj.pathname.split('.').pop();
-      const mime_type = mime_types[file_extension];
-      if (mime_type) {
-        res.setHeader('content-type', mime_type);
+      const fileExtension = urlObj.pathname.split('.').pop()
+      const mimeType = mimeTypes[fileExtension]
+      if (mimeType) {
+        res.setHeader('content-type', mimeType)
       }
-      res.writeHead(200);
-      res.end(data);
+      res.writeHead(200)
+      res.end(data)
     }
-  });
-};
+  })
+}
 
 on('GET', '/api', (req, res) => {
-  res.writeHead(200);
-  res.end('hello api\n');
-});
+  res.writeHead(200)
+  res.end('hello api\n')
+})
 
 on('GET', /\/api\/files\/.*/, (req, res) => {
-  res.writeHead(200);
-  res.end('hello files\n');
-});
+  res.writeHead(200)
+  res.end('hello files\n')
+})
 
 // TODO
-// Pass url_obj rather than generate it twice
+// Pass urlObj rather than generate it twice
 // Allow request handlers to see the server and subdomain
-const request_handler = (req, res) => {
-  const url_obj = url.parse(req.url);
-  // console.log(req.method, url_obj.pathname, url_obj.query);
-  const handler = handler_map.find(
-    handler => handler.test(url_obj.pathname) && handler.methods.indexOf(req.method) !== -1
-  ) || handle_static_request;
-  handler(req, res);
-};
+const requestHandler = (req, res) => {
+  const urlObj = url.parse(req.url)
+  // console.log(req.method, urlObj.pathname, urlObj.query);
+  const handler = handlerMap.find(
+    handler => handler.test(urlObj.pathname) && handler.methods.indexOf(req.method) !== -1
+  ) || handleStaticRequest
+  handler(req, res)
+}
 
 if (settings.https) {
-  https.createServer(options, request_handler).listen(settings.socket);
+  https.createServer(options, requestHandler).listen(settings.socket)
 } else {
-  http.createServer({}, request_handler).listen(settings.socket);
+  http.createServer({}, requestHandler).listen(settings.socket)
 }
