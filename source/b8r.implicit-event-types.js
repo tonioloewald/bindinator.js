b0cd743e938a851b4b62d7cc08c57d637eeedf13
Tonio Loewald <tonio@loewald.com>
2019-06-01 00:14:35 -0700

    documenting event types handled by default
    added implicit-event-types to documentation
    added some documentation to implicit-event-types

--- source/b8r.implicit-event-types.js
+++ source/b8r.implicit-event-types.js
@@ -1,6 +1,36 @@
+/**
+# Implicit Event Types
+
+These are the event types which b8r handles by default.
+
+To handle other types of events, you can call `b8r.implicitlyHandleEventsOfType('type')`
+
+## Mouse Events
+
+- `mousedown`, `mouseup`, `click`, `dblclick`, `contextmenu`
+- `mouseleave`, `mouseenter`, `mousemove`, `mouseover`, `mouseout`
+- `mousewheel`, `scroll`
+
+## Drag Events
+
+- `dragstart`, `dragenter`, `dragover`, `dragleave`, `dragend`, `drop`
+
+## CSS Animations
+
+- `transitionend`, `animationend`
+
+## User Input
+
+- `keydown`, `keyup`
+- `input`, `change`
+- `cut`, `copy`, `paste`
+- `focus`, `blur`
+
+*/
+
 export default [
-  'mousedown', 'mouseup', 'mousemove', 'mouseover', 'mouseout', 'click', 'dblclick',
-  'mouseleave', 'mouseenter',
+  'mousedown', 'mouseup', 'click', 'dblclick',
+  'mouseleave', 'mouseenter', 'mousemove', 'mouseover', 'mouseout',
   'mousewheel', 'scroll', // FIXEME passive?!
   'contextmenu',
   'dragstart', 'dragenter', 'dragover', 'dragleave', 'dragend', 'drop',
