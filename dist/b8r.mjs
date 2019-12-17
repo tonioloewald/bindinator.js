2edb8cb969352a1c61d6f7bc17908492428dc8c5
Tonio Loewald <tonio@loewald.com>
2019-12-16 22:03:25 -0800

    initialValue improvements
    - a modern component with initialValue no longer inherits properties from its parent
    - initialValue may now be an async function

--- dist/b8r.mjs
+++ dist/b8r.mjs
@@ -7107,12 +7107,11 @@ b8r.insertComponent = async function (component, element, data) {
   const findOneWithin = selector => b8r.findWithin(element, selector);
   const findOne = selector => b8r.findOneWithin(element, selector);
   const initialValue = typeof component.initialValue === 'function'
-    ? component.initialValue({ b8r, get, set, touch, on, component: element, findOne, findOneWithin })
+    ? await component.initialValue({ b8r, get, set, touch, on, component: element, findOne, findOneWithin })
     : b8r.deepClone(component.initialValue);
   data = {
-    ...data,
+    ...(initialValue || data),
     dataPath,
-    ...(initialValue || {}),
     componentId
   };
   if (component.load) {
