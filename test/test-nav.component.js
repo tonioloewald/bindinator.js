cedcc535ba5a412145c49069bb939beef7354afa
Tonio Loewald <tonio@loewald.com>
2020-01-30 09:19:31 -0800

    rearranged tests and benchmarks
    - benchmark pages now available within the documentation system

---
+++ test/test-nav.component.js
@@ -0,0 +1,9 @@
+export default {
+  html: `
+  <div>
+  <a href="unit-tests.html">unit tests</a> |
+    <a href="benchmark.html">b8r</a> |
+    <a href="benchmark-data-table.html">b8r data-table</a> |
+    <a href="benchmark-vanillajs.html">vanillajs</a>
+  </div>`
+}
