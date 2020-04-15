004bb8b27a9b1d099631eda90f6ad00529584c05
Tonio Loewald <tonio@loewald.com>
2020-04-15 10:32:43 -0700

    Update graph.component.js
    - can switch types
    - replaced title with aria-label (which actually works, at least in chrome)

--- components/graph.component.js
+++ components/graph.component.js
@@ -2,10 +2,13 @@
 # graph component
 
 ```
-<select data-bind="value=_component_.type">
-    <option>line</option>
-    <option>bar</option>
-</select><br>
+<label>
+    <span>Graph Type</span>
+    <b8r-select-bar data-bind="value=_component_.type">
+        <b8r-option value="line">Line</b8r-option>
+        <b8r-option value="bar">Bar</b8r-option>
+    </b8r-select-bar>
+</label><br>
 <b8r-component path="../components/graph.component.js" data-bind="component(type)=_component_.type"></b8r-component>
 <script>
     set('type', 'bar')
@@ -13,21 +16,23 @@
 ```
 */
 
+import('../web-components/select.js')
+
 const svgns = "http://www.w3.org/2000/svg"
 const makeRect = () => document.createElementNS(svgns, 'rect')
 
 export default {
     css: `
     ._component_ {
-    --background-color: rgba(255,255,255,0.75);
-    --marking-color: rgba(0,0,0,0.25);
-    --bar-color: rgba(255,0,0,0.5);
-    --line-width: 2;
-    --font-family: Helvetica, Sans-serif;
-    --text-color: rgba(0,0,0,0.5);
-    display: inline-block;
-    background-color: var(--background-color);
-    padding: 20px;
+        --background-color: rgba(255,255,255,0.75);
+        --marking-color: rgba(0,0,0,0.25);
+        --bar-color: rgba(255,0,0,0.5);
+        --line-width: 2;
+        --font-family: Helvetica, Sans-serif;
+        --text-color: rgba(0,0,0,0.5);
+        display: inline-block;
+        background-color: var(--background-color);
+        padding: 20px;
     }
 
     ._component_ h4 {
@@ -38,7 +43,7 @@ export default {
     <h4 data-bind="text=_component_.title"></h4>
     <svg data-bind="method(_component_.viewbox)=_component_.width,_component_.height" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <g fill="var(--marking-color)">
-            <rect class="Rectangle" x="0" y="49" data-bind="attr(width)=_component_.width" height="1"></rect>
+            <rect x="0" y="49" data-bind="attr(width)=_component_.width" height="1"></rect>
         </g>
         <g data-list="_component_.data" fill="var(--bar-color)" data-bind="show_if(bar)=_component_.type;method(_component_.bars)=.">
         </g>
@@ -77,6 +82,7 @@ export default {
             return Math.min(...(values || [0, ...data.map(datum => datum.cells).flat()]))
         },
         viewbox(elt, [width, height]) {
+            // offsets to prevent clipping at edges; TODO make them configurable
             elt.setAttribute('viewBox', `${-5} ${-5} ${width + 10} ${height + 10}`)
             elt.style.margin = '-5px'
             elt.setAttribute('width', width + 'px')
@@ -95,7 +101,7 @@ export default {
                 bar.setAttribute('height', barHeight)
                 bar.setAttribute('width', barWidth - barSpacing)
                 bar.setAttribute('fill', color)
-                bar.setAttribute('title', `${name}: ${value} at ${axis[idx]}`)
+                bar.setAttribute('aria-label', `${name}: ${value} at ${axis[idx]}`)
                 element.append(bar)
             })
         },
@@ -107,7 +113,7 @@ export default {
             const size = cells.length - 1
             const points = cells.map(v => v/max).map(x => height - x * height).map((x, idx) => `${idx / size * width},${x}`).join(' ')
             element.setAttribute('points', points)
-            element.setAttribute('title', name)
+            element.setAttribute('aria-label', name)
             if(color) element.setAttribute('stroke', color)
             if(strokeWidth) element.setAttribute('stroke-width', strokeWidth)
         },
