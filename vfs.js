60a0d8b4d1d591b692c984694b3a25eff0a12203
Tonio Loewald <tonio@loewald.com>
2020-05-03 16:23:43 -0700

    Docs

--- vfs.js
+++ vfs.js
@@ -13,12 +13,40 @@ If follows that:
 
 GET /vsf/* returns the whole virtual tree.
 
+## Why?!
+
+This all started because I wanted the [fiddle](.?source=fiddle.component.html) 
+component to handle the new ES6-based Javascript components, which are 
+`export`ed from an ES6 module as an object constant. You can't `import()` 
+code programmatically without some kind of 
+[glorious hack](https://2ality.com/2019/10/eval-via-import.html).
+
+However, it's still a hack. It doesn't handle transitive relative dependencies
+(and in fact this bit me with [the first component I tried it on](.?source=components/analog-clock.component.js))
+which uses [dom-timers](.?source=lib/dom-timers.js).
+
+This allows fiddles to create virtual files in a local service layer (i.e. `./vfs/`)
+and then `import()` them as though they were "out there". In order for this to work
+properly, `vfs` even gives the files appropriate mime-types, and so forth.
+
+Ultimately, it would allow you to mount a file system inferred from a service 
+layer (e.g. github) and, for example, allow a web-based IDE to virtually serve
+itself a project from a web-based repository with no actual file-system.
+
 ## Note
 
 Service workers *do not work on first page load*. Consequently, `vfs`
 will *immediately* force a page reload if it sees that the controller is null.
 It follows that if you want to use `vfs` you should load it as soon as
 possible to avoid rendering the user interface twice.
+
+> ## If tests are failing…
+>
+> Right now, `vfs` does not force automatic updates so you may need to force
+> the latest version of the vfs service worker to load. Usually a forced
+> (shift) refresh with cache-busting will do the trick, but you may need
+> to explicitly kill the service worker, e.g. `chrome://inspect/#service-workers`
+> in Chrome.
 ~~~~
 // version check
 Test(() => b8r.ajax('/vfs/version')).shouldBe('0.1')
