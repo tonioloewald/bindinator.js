ce70dd38740a78b7056d834037c6607833e1cca7
Tonio Loewald <tonio@loewald.com>
2020-05-22 17:10:09 -0700

    WIP showcase component

--- components/analog-clock.component.js
+++ components/analog-clock.component.js
@@ -10,6 +10,110 @@
 Here's a pure JS version of the old analog clock component.
 */
 
+const colors = [
+  { description: 'very dark grey', value: '#222' },
+  { description: 'dark grey', value: '#444' },
+  { description: 'grey', value: '#666' },
+  { description: 'light grey', value: '#ddd' },
+  { description: 'off-white', value: '#f7f7f7' },
+  { description: 'red', value: 'blue' },
+  { description: 'green', value: 'blue' },
+  { description: 'blue', value: 'blue' },
+];
+
+const timePartsRegEx = /\d+|AM|PM/g
+
+const localTime = () => {
+  return new Date().toLocaleString('en-US').split(',').pop().match(timePartsRegEx)
+}
+
+const fiveSecondsToMidnight = () => [11,59,55,'PM']
+
+const drinkOClock = () => [5,0,15,'PM']
+
+export const timeFromTimezone = timezone => {
+  return () => () => new Date().toLocaleString('en-US', {timeZone}).split(',').pop().match(timePartsRegEx)
+}
+
+const zuluTime = () => timeFromTimezone('Zulu')
+const sydneyTime = () => timeFromTimezone('Australia/Sydney')
+
+export const configuration = [
+  {
+    name: 'faceColor',
+    values: colors,
+  },
+  {
+    name: 'tickColor',
+    values: colors,
+  },
+  {
+    name: 'handColor',
+    values: colors,
+  },
+  {
+    name: 'secondHandColor',
+    values: colors,
+  },
+  {
+    name: 'logoUrl',
+    values: [
+      { description: 'no logo', value: null },
+      { description: 'b8r logo greyscale', value: 'images/bindinator-logo-notext-mono.svg' },
+    ],
+  },
+  {
+    name: 'getHMS',
+    values: [
+      { description: 'local time', value: localTime },
+      { description: 'time in Sydney', value: sydneyTime },
+      { description: 'GMT', value: zuluTime },
+      { description: 'five seconds to midnight', value: fiveSecondsToMidnight },
+      { description: 'it’s 5PM somewhere…', value: drinkOClock },
+    ]
+  },
+]
+
+export const examples = [
+  {
+    name: 'local time',
+    skipSnapshot: true,
+  },
+  {
+    skipSnapshot: true,
+    name: 'no logo',
+    settings: [
+      { name: 'logoUrl', option: 0 }
+    ]
+  },
+  {
+    name: 'Sydney Time',
+    skipSnapshot: true,
+    settings: [
+      { name: 'getHMS', option: 2 }
+    ],
+  },
+  {
+    name: 'GMT',
+    skipSnapshot: true,
+    settings: [
+      { name: 'getHMS', option: 2 }
+    ],
+  },
+  {
+    name: 'fixed time (5PM)',
+    settings: [
+      { name: 'getHMS', option: 3 }
+    ],
+  },
+  {
+    name: 'fixed time (5 seconds to midnight)',
+    settings: [
+      { name: 'getHMS', option: 4 }
+    ],
+  },
+]
+
 export default {
   css: `
     ._component_ canvas { width: 128px; height: 128px; }
@@ -21,17 +125,29 @@ export default {
       "
       width="256" height="256"
     ></canvas>`,
+  initialValue: {
+    faceColor: '#f7f7f7',
+    tickColor: '#222',
+    handColor: '#444',
+    secondHandColor: 'red',
+    logoUrl: 'images/bindinator-logo-notext-mono.svg',
+    getHMS: localTime,
+  },
   load: async ({ get, set, findOne }) => {
     /* global Image */
     const { domInterval } = await import('../lib/dom-timers.js')
+    const { getHMS, faceColor, tickColor, handColor, secondHandColor, logoUrl } = get()
 
     const clock = findOne('canvas')
     const g = clock.getContext('2d')
-    const logo = new Image()
-    logo.src = 'images/bindinator-logo-notext-mono.svg'
-    g.fillStyle = '#f7f7f7'
+    let logo = false
+    if (logoUrl) {
+      logo = new Image()
+      logo.src = logoUrl
+    }
+    g.fillStyle = faceColor
     const d2r = Math.PI / 180
-    const drawRay = (width, angle, r0, r1, color = '#222') => {
+    const drawRay = (width, angle, r0, r1, color = tickColor) => {
       g.strokeStyle = color
       g.lineWidth = width
       const radians = angle * d2r
@@ -48,8 +164,8 @@ export default {
     }
 
     const update = () => {
-      const now = new Date()
-      const timeText = now.toLocaleTimeString()
+      const [hours, minutes, seconds, pm] = getHMS()
+      const timeText = `${hours}:${minutes}:${seconds} ${pm}`
       if (timeText !== get('time')) {
         set('time', timeText)
         g.clearRect(0, 0, 256, 256)
@@ -58,14 +174,14 @@ export default {
         g.fill()
         g.drawImage(logo, 108, 160, 40, 29)
         for (let h = 0; h < 12; h++) {
-          drawRay(6, h * 30, 96, 120, '#444')
+          drawRay(6, h * 30, 96, 120, handColor)
         }
-        const hourHand = (now.getHours() % 12 * 30) + now.getMinutes() * 0.5
-        const minuteHand = now.getMinutes() * 6 + now.getSeconds() * 0.1
-        const secondHand = now.getSeconds() * 6 // + now.getMilliseconds() * 0.006;
+        const hourHand = (parseInt(hours,10) % 12 * 30) + parseInt(minutes,10) * 0.5
+        const minuteHand = parseInt(minutes,10) * 6 + parseInt(seconds,10) * 0.1
+        const secondHand = parseInt(seconds,10) * 6 // + now.getMilliseconds() * 0.006;
         drawRay(6, hourHand, 0, 80)
         drawRay(4, minuteHand, 0, 116)
-        drawRay(2, secondHand, 0, 116, 'red')
+        drawRay(2, secondHand, 0, 116, secondHandColor)
       }
     }
     domInterval(clock, update, 16.6)
---
+++ components/showcase.component.js
@@ -0,0 +1,118 @@
+/**
+# Showcase
+## Component Examples, Demos, and Tests
+
+This component is designed to showcase and test components. It works best with
+the new javascript components.
+
+Usage:
+
+    <b8r-component 
+      path="path/to/component-test.component.js"
+      data-module="./path/to/foo.component.js"
+    ></b8r-component>
+
+## Configuration
+
+If a javascript component module exports a `configuration` array, `component-test`
+will provide a configuration panel for interactively modifying the displayed component.
+
+The configuration object looks like this:
+
+    export const configuration = {
+      {
+        name: 'size',
+        values: [
+          { description: 'small', value: 'small' },
+          { description: 'normal', value: 'normal' },
+          ...
+        ]
+      },
+      {
+        name: 'animation',
+        values: [
+          { description: 'animated', value: true },
+          { description: 'no animation', value: false },
+          ...
+        ]
+      },
+      ...
+    }
+
+## Examples
+
+If a javascript component module exports an `examples` array, `component-test`
+will allow the user to see the different examples (all at once or one-at-a-time).
+
+The examples array looks like this:
+
+    export const examples = [
+      {
+        name: 'default',
+        skipSnapshot: true,
+      },
+      {
+        name: 'no animation',
+        settings: [
+          { name: 'animation', option: false },
+          ...
+        ]
+      },
+    ]
+
+Within an example `skipSnapshot` blocks snapshot testing for a given example. In
+this case we're assuming that the component is normally *animated* and snapshot
+testing will be highly prone to fail.
+
+(If no `examples` array is exported, `component-test` will simply display the component
+in its default state.)
+
+Each example provided (or just the default state) will be **snapshot-tested**.
+
+## Snapshot Tests
+### (NOT IMPLEMENTED)
+
+`component-test` performs snapshot testing when you run the `b8r` project on
+`localhost` using the provided dev server (`server.js`).
+
+A snapshot test comprises rendering the component in a given state with `puppeteer`
+and comparing the result to a saved "golden" snapshot. If the results don't match
+(or if there's no result) the problem will be flagged and you can opt to update
+the "golden" snapshot, skip snapshot testing for that particular configuration,
+or fix your code.
+
+### Dark Mode vs. Light Mode
+
+On macOS, the snapshot tests will test both "light" and "dark" modes.
+
+## a11y Tests 
+### (NOT IMPLEMENTED)
+
+`component-test` performs `ally` testing using [aXe](https://github.com/dequelabs/axe-core)
+on each example and interactively displays any issues it discovers.
+
+Your module can export an `a11yException` object to selectively suppress a11y errors.
+
+You can examine a list of accessibility issues and mark those you wish to ignore
+for the component as a whole or any specific case, and you will be provided with 
+an `a11yExceptions` object to add to your component source file.
+
+`component-test` will flag if you are passing tests you have opted to ignore (so it
+will not progression as well as regression).
+
+## Web Components
+### (NOT IMPLEMENTED)
+
+## Legacy Components
+
+For old HTML components, `component-test` will simply display the component
+in its default state.
+
+You can specify a helper javascript module using the `data-helper` attribute,
+which will attempt to load `configuration`, `examples`, and `a11yExceptions`.
+ 
+In order to derive the most benefit from component-test
+*/
+export default {
+
+}
