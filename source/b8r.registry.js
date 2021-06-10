8c731bd991986a183609f29cc1733c59cf197bfd
Tonio Loewald <tonio@loewald.com>
2021-06-09 17:16:10 -0700

    update registry docs

--- source/b8r.registry.js
+++ source/b8r.registry.js
@@ -27,8 +27,8 @@ always wanted. Thank you to [Steven Williams](https://www.linkedin.com/in/steven
 for the suggestion.
 
 `b8r.reg` is a proxy for its `registry`, providing syntax-sugar for `get` and `set` via
-an ES6 Proxy. The way the proxy works is that it gives you proxies for its object properties,
-and so on (recursively), each proxy knowing the path of the value it wraps.
+an ES6 Proxy. The registry object is wrapped with a proxy that returns its object properties as
+proxies, and so on (recursively), each knowing the path of the value it wraps.
 
     import {reg} from 'path/to/b8r.js'
     const obj = {
@@ -453,11 +453,18 @@ lists).
 
 > ### Note
 >
-> Having gained experience with the framework, I am doubling down
-> on object paths and simplifying the API in favor of:
+> I'm always looking at ways to streamline `b8r` code, usually without breaking
+> anything, and ideally making the resulting code easier to read, write, and grok.
+>
 > <pre>
-> b8r.get('path.to.value');
-> b8r.set('path.to.value', new_value);
+> b8r.getByPath('path', 'to.value')                // original
+> b8r.setByPath('path', 'to.value', new_value)
+>
+> b8r.get('path.to.value')                         // simplified
+> b8r.set('path.to.value', new_value)
+>
+> b8r.reg.path.to.value                            // using Proxy
+> b8r.reg.path.to.value = new_value
 > </pre>
 > The older APIs (setByPath, etc.) will ultimately be deprecated. Even now they
 > are little more than wrappers for set/get. See the *Registry* docs.
